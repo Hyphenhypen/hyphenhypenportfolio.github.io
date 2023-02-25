8c595a80bba5e6ab050f8c81ab0db26b1500085c
Hyphenhypen <adityapratapsjnhh7654@gmail.com>
2023-02-25 19:09:12 +0530

    This is my first commit.

---
+++ main.js
@@ -0,0 +1,7 @@
+var typed = new Typed('.multiple-texts', {
+    strings: ['FrontEnd Web Developer', 'Youtuber', 'Freelancer'],
+    typeSpeed: 100,
+    backSpeed: 100,
+    baclDelay: 1000,
+    loop: true
+})
